05184b985300110a5ed1ac77d95ae9456d81f4d7
Maryna Boholiubova <astoret2015@gmail.com>
2026-05-11 21:14:26 +0200

    fix interfaces

--- src/types/movie.ts
+++ src/types/movie.ts
@@ -1,36 +1,10 @@
-import axios from 'axios';
-import type { Movie } from '../types/movie';
-
-export interface MoviesResponse {
-  page: number;
-  results: Movie[];
-  total_pages: number;
-  total_results: number;
+export interface Movie {
+  id: number;
+  title: string;
+  overview: string;
+  poster_path: string;
+  backdrop_path: string;
+  release_date: string;
+  vote_average: number;
 }
-
-interface FetchMoviesParams {
-  query: string;
-  page: number;
-}
-
-export const fetchMovies = async ({
-  query,
-  page,
-}: FetchMoviesParams): Promise<MoviesResponse> => {
-  const response = await axios.get<MoviesResponse>(
-    'https://api.themoviedb.org/3/search/movie',
-    {
-      params: {
-        query,
-        page,
-      },
-      headers: {
-        Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
-      },
-    }
-  );
-
-  return response.data;
-};
-
 
